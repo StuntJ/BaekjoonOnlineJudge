4c5a4c64be7261f3417627a6647073cc0b5e9e67
StuntJ <95505685+StuntJ@users.noreply.github.com>
2024-12-28 21:58:37 +0900

    [Gold III] Title: 색상환, Time: 4 ms, Memory: 9848 KB -BaekjoonHub

--- 백준/Gold/2482. 색상환/색상환.cc
+++ 백준/Gold/2482. 색상환/색상환.cc
@@ -1,51 +1,35 @@
 #include <bits/stdc++.h>
+#define MAX_N 1001
 #define DIV 1000000003
+
 using namespace std;
 
 typedef long long ll;
-
-vector<vector<vector<vector<ll>>>> cache;
-int N, K;
-
-ll solve(int n, int k, int bf, int b)
-{
-	if (b == 0 && n == N)
-	{
-		if (k == K) return 1;
-		else return 0;
-	}
-	if (b == 1 && n == N - 1)
-	{
-		if (k == K)	return 1;
-		else return 0;
-	}
-	if (k > K) return 0;
-	ll& ret = cache[n][k][bf][b];
-	if (ret != -1) return ret;
-	ret = 0;
-	if(bf==0) ret += solve(n + 1, k + 1, 1, b);
-	ret %= DIV;
-	ret += solve(n + 1, k, 0, b);
-	ret %= DIV;
-	return ret;
-}
+ll dp[MAX_N][MAX_N];
 
 int main()
 {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
+	int N, K;
 	cin >> N >> K;
-	if (K * 2 > N)
+
+	for (int i = 0; i <= N; i++)
 	{
-		cout << 0;
-		return 0;
+		dp[i][0] = 1;
+		dp[i][1] = i;
 	}
 
-	cache = vector<vector<vector<vector<ll>>>>(N + 1, vector<vector<vector<ll>>>(K + 1, vector<vector<ll>>(2, vector<ll>(2, -1))));
+	for (int i = 2; i <= N; i++)
+	{
+		for (int j = 2; j <= K; j++)
+		{
+			dp[i][j] = dp[i - 2][j - 1] + dp[i - 1][j];
+			dp[i][j] %= DIV;
+		}
+	}
 
-	ll ans1 = solve(1, 0, 0, 0);
-	ans1 += solve(1, 1, 1, 1);
-	ans1 %= DIV;
-	cout << ans1;
+	ll ans = dp[N - 3][K - 1] + dp[N - 1][K];
+	cout << ans % DIV;
 }
