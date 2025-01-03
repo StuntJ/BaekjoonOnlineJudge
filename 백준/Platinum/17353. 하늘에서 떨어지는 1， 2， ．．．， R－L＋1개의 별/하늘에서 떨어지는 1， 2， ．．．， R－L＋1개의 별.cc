e039f6c9d60f372c82bbf930c0e41d89577d1a3f
StuntJ <95505685+StuntJ@users.noreply.github.com>
2025-01-03 22:10:10 +0900

    [Platinum II] Title: 하늘에서 떨어지는 1, 2, ..., R-L+1개의 별, Time: 40 ms, Memory: 4372 KB -BaekjoonHub

--- 백준/Platinum/17353. 하늘에서 떨어지는 1， 2， ．．．， R－L＋1개의 별/하늘에서 떨어지는 1， 2， ．．．， R－L＋1개의 별.cc
+++ 백준/Platinum/17353. 하늘에서 떨어지는 1， 2， ．．．， R－L＋1개의 별/하늘에서 떨어지는 1， 2， ．．．， R－L＋1개의 별.cc
@@ -3,118 +3,79 @@
 using namespace std;
 typedef long long ll;
 
-struct LSQ
+struct FenwickTree
 {
-	int n;
-	vector<ll> rangeSum;
-	vector<ll> lazy;
-
-	LSQ(const vector<ll>& array)
+	vector<ll> rangeSum, count;
+	FenwickTree(ll n)
 	{
-		n = array.size();
-		rangeSum.resize(n * 4);
-		lazy.assign(n * 4, 0);
-		init(array, 1, 0, n - 1);
+		rangeSum.resize(n + 1);
+		count.resize(n + 1);
 	}
 
-	ll init(const vector<ll>& array, int node, int nodeLeft, int nodeRight)
+	pair<ll, ll> sum(ll pos)
 	{
-		if (nodeLeft == nodeRight)
+		pos++;
+		pair<ll, ll> ret = { 0,0 };
+		while (pos > 0)
 		{
-			if (nodeLeft == 0) return rangeSum[node] = array[nodeLeft];
-			else return rangeSum[node] = array[nodeLeft] - array[nodeLeft - 1];
+			ret.first += rangeSum[pos];
+			ret.second += count[pos];
+			pos &= (pos - 1);
 		}
-		int mid = (nodeLeft + nodeRight) / 2;
-		ll leftSum = init(array, node * 2, nodeLeft, mid);
-		ll rightSum = init(array, node * 2 + 1, mid + 1, nodeRight);
-		return rangeSum[node] = leftSum + rightSum;
+		return ret;
 	}
 
-	void lazyUpdate(int node, int left, int right)
+	void update(ll pos, ll val)
 	{
-		if (lazy[node] != 0)
+		pos++;
+		while (pos < rangeSum.size())
 		{
-			rangeSum[node] += (right - left + 1) * lazy[node];
-			if (left != right)
-			{
-				lazy[node * 2] += lazy[node];
-				lazy[node * 2 + 1] += lazy[node];
-			}
-			lazy[node] = 0;
+			count[pos] += ((val >= 0) ? 1 : -1);
+			rangeSum[pos] += val;
+			pos += (pos & -pos);
 		}
 	}
 
-	ll query(int left, int right, int node, int nodeLeft, int nodeRight)
+	void updateTotal(ll left, ll right, ll val)
 	{
-		lazyUpdate(node, nodeLeft, nodeRight);
-		if (nodeRight < left || right < nodeLeft) return 0;
-		if (left <= nodeLeft && nodeRight <= right) return rangeSum[node];
-		int mid = (nodeLeft + nodeRight) / 2;
-		return query(left, right, node * 2, nodeLeft, mid) + query(left, right, node * 2 + 1, mid + 1, nodeRight);
-	}
-
-	long long query(int x)
-	{
-		return query(0, x, 1, 0, n - 1);
-	}
-
-	void update(int left, int right, ll val, int node, int nodeLeft, int nodeRight)
-	{
-		lazyUpdate(node, nodeLeft, nodeRight);
-		if (nodeRight < left || right < nodeLeft) return;
-		if (left <= nodeLeft && nodeRight <= right)
-		{
-			rangeSum[node] += val * (nodeRight - nodeLeft + 1);
-			if (nodeLeft != nodeRight)
-			{
-				lazy[node * 2] += val;
-				lazy[node * 2 + 1] += val;
-			}
-			return;
-		}
-		int mid = (nodeLeft + nodeRight) / 2;
-		update(left, right, val, node * 2, nodeLeft, mid);
-		update(left, right, val, node * 2 + 1, mid + 1, nodeRight);
-		rangeSum[node] = rangeSum[node * 2] + rangeSum[node * 2 + 1];
-	}
-
-	void update(int left, int right, ll val)
-	{
-		update(left, right, val, 1, 0, n - 1);
-		if (right < n - 1) update(right + 1, right + 1, -(right - left + 1), 1, 0, n - 1);
+		update(left, val);
+		if (right < rangeSum.size() - 2) update(right + 1, -val);
 	}
 };
 
 int main()
 {
 	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
+	cin.tie(NULL);
 
-	int N;
+	ll N;
 	cin >> N;
 	vector<ll> arr(N);
 	for (auto& i : arr) cin >> i;
 
-	LSQ seg(arr);
+	FenwickTree seg(N);
 
-	int Q;
+	ll Q;
 	cin >> Q;
+
 	while (Q--)
 	{
-		int qu;
+		ll qu;
 		cin >> qu;
 		if (qu == 1)
 		{
-			int L, R;
+			ll L, R;
 			cin >> L >> R;
-			seg.update(L - 1, R - 1, 1);
+			seg.updateTotal(L - 1, R - 1, L);
 		}
 		else
 		{
 			ll x;
 			cin >> x;
-			cout << seg.query(x - 1) << '\n';
+			pair<ll, ll> ans = seg.sum(x - 1);
+			//cout << ans.first << ' ' << ans.second << '\n';
+			ll res = ans.second * (x + 1) - ans.first + arr[x - 1];
+			cout << res << '\n';
 		}
 	}
 }
