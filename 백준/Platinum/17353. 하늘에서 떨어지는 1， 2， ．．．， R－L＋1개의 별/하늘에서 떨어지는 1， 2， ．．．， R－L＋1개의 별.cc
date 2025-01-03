2f784fe60091328989466b5fa968157fc05f994d
StuntJ <95505685+StuntJ@users.noreply.github.com>
2025-01-03 20:30:22 +0900

    [Platinum II] Title: 하늘에서 떨어지는 1, 2, ..., R-L+1개의 별, Time: 96 ms, Memory: 9068 KB -BaekjoonHub

--- 백준/Platinum/17353. 하늘에서 떨어지는 1， 2， ．．．， R－L＋1개의 별/하늘에서 떨어지는 1， 2， ．．．， R－L＋1개의 별.cc
+++ 백준/Platinum/17353. 하늘에서 떨어지는 1， 2， ．．．， R－L＋1개의 별/하늘에서 떨어지는 1， 2， ．．．， R－L＋1개의 별.cc
@@ -6,23 +6,24 @@ typedef long long ll;
 struct LSQ
 {
 	int n;
-	vector<ll> pSum;
 	vector<ll> rangeSum;
 	vector<ll> lazy;
-	vector<ll> sub;
 
 	LSQ(const vector<ll>& array)
 	{
 		n = array.size();
 		rangeSum.resize(n * 4);
 		lazy.assign(n * 4, 0);
-		sub.assign(n * 4, 0);
-		init(array, 1, 1, n - 1);
+		init(array, 1, 0, n - 1);
 	}
 
 	ll init(const vector<ll>& array, int node, int nodeLeft, int nodeRight)
 	{
-		if (nodeLeft == nodeRight) return rangeSum[node] = array[nodeLeft];
+		if (nodeLeft == nodeRight)
+		{
+			if (nodeLeft == 0) return rangeSum[node] = array[nodeLeft];
+			else return rangeSum[node] = array[nodeLeft] - array[nodeLeft - 1];
+		}
 		int mid = (nodeLeft + nodeRight) / 2;
 		ll leftSum = init(array, node * 2, nodeLeft, mid);
 		ll rightSum = init(array, node * 2 + 1, mid + 1, nodeRight);
@@ -33,19 +34,13 @@ struct LSQ
 	{
 		if (lazy[node] != 0)
 		{
-			ll num = right - left + 1;
-			ll a = lazy[node];
-			ll d = sub[node];
-			rangeSum[node] += num * (2 * a + (num - 1) * d) / 2;
-
+			rangeSum[node] += (right - left + 1) * lazy[node];
 			if (left != right)
 			{
 				lazy[node * 2] += lazy[node];
-				lazy[node * 2 + 1] += lazy[node] + ((left + right) / 2 - left + 1) * sub[node];
-				sub[node * 2] += sub[node];
-				sub[node * 2 + 1] += sub[node];
+				lazy[node * 2 + 1] += lazy[node];
 			}
-			lazy[node] = sub[node] = 0;
+			lazy[node] = 0;
 		}
 	}
 
@@ -58,39 +53,35 @@ struct LSQ
 		return query(left, right, node * 2, nodeLeft, mid) + query(left, right, node * 2 + 1, mid + 1, nodeRight);
 	}
 
-	long long query(int k)
+	long long query(int x)
 	{
-		return query(k, k, 1, 1, n - 1);
+		return query(0, x, 1, 0, n - 1);
 	}
 
-	void update(int left, int right, int node, int nodeLeft, int nodeRight)
+	void update(int left, int right, ll val, int node, int nodeLeft, int nodeRight)
 	{
 		lazyUpdate(node, nodeLeft, nodeRight);
 		if (nodeRight < left || right < nodeLeft) return;
 		if (left <= nodeLeft && nodeRight <= right)
 		{
-			ll num = nodeRight - nodeLeft + 1;
-			ll a = nodeLeft - left + 1;
-			ll d = 1;
-			rangeSum[node] += num * (2 * a + (num - 1) * d) / 2;
+			rangeSum[node] += val * (nodeRight - nodeLeft + 1);
 			if (nodeLeft != nodeRight)
 			{
-				lazy[node * 2] += a;
-				lazy[node * 2 + 1] += a + (nodeLeft + nodeRight) / 2 - nodeLeft + 1;
-				sub[node * 2]++;
-				sub[node * 2 + 1]++;
+				lazy[node * 2] += val;
+				lazy[node * 2 + 1] += val;
 			}
 			return;
 		}
 		int mid = (nodeLeft + nodeRight) / 2;
-		update(left, right, node * 2, nodeLeft, mid);
-		update(left, right, node * 2 + 1, mid + 1, nodeRight);
+		update(left, right, val, node * 2, nodeLeft, mid);
+		update(left, right, val, node * 2 + 1, mid + 1, nodeRight);
 		rangeSum[node] = rangeSum[node * 2] + rangeSum[node * 2 + 1];
 	}
 
-	void update(int left, int right)
+	void update(int left, int right, ll val)
 	{
-		update(left, right, 1, 1, n - 1);
+		update(left, right, val, 1, 0, n - 1);
+		if (right < n - 1) update(right + 1, right + 1, -(right - left + 1), 1, 0, n - 1);
 	}
 };
 
@@ -98,33 +89,32 @@ int main()
 {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
+	cout.tie(0);
 
 	int N;
 	cin >> N;
+	vector<ll> arr(N);
+	for (auto& i : arr) cin >> i;
 
-	vector<ll> v(N+1);
-
-	for (int i = 1; i <= N; i++) cin >> v[i];
-
-	LSQ seg(v);
+	LSQ seg(arr);
 
 	int Q;
 	cin >> Q;
 	while (Q--)
 	{
-		int type;
-		cin >> type;
-		if (type == 1)
+		int qu;
+		cin >> qu;
+		if (qu == 1)
 		{
-			int l, r;
-			cin >> l >> r;
-			seg.update(l, r);
+			int L, R;
+			cin >> L >> R;
+			seg.update(L - 1, R - 1, 1);
 		}
 		else
 		{
-			int k;
-			cin >> k;
-			cout << seg.query(k) << '\n';
+			ll x;
+			cin >> x;
+			cout << seg.query(x - 1) << '\n';
 		}
 	}
 }
