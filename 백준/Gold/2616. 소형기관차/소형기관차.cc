237331b59f009430444176ff12eb54e1ab6a552e
StuntJ <95505685+StuntJ@users.noreply.github.com>
2025-01-05 10:14:03 +0900

    [Gold III] Title: 소형기관차, Time: 4 ms, Memory: 5168 KB -BaekjoonHub

--- 백준/Gold/2616. 소형기관차/소형기관차.cc
+++ 백준/Gold/2616. 소형기관차/소형기관차.cc
@@ -11,7 +11,12 @@ int main()
 	cin >> N;
 
 	vector<int> v(N + 1);
-	for (int i = 1; i <= N; i++) cin >> v[i];
+	vector<int> pSum(N + 1, 0);
+	for (int i = 1; i <= N; i++)
+	{
+		cin >> v[i];
+		pSum[i] = pSum[i - 1] + v[i];
+	}
 
 	vector<vector<int>> dp(N + 1, vector<int>(4, 0));
 	int M;
@@ -21,16 +26,7 @@ int main()
 	{
 		for (int j = 0; j < 4; j++)
 		{
-			int ret = 0;
-			if (i - M >= 0 && j > 0)
-			{
-				ret = dp[i - M][j - 1];
-				for (int k = i - M + 1; k <= i; k++)
-				{
-					ret += v[k];
-				}
-			}
-			dp[i][j] = max(ret, dp[i][j]);
+			if (i - M >= 0 && j > 0) dp[i][j] = dp[i - M][j - 1] + pSum[i] - pSum[i - M];
 			dp[i][j] = max(dp[i][j], dp[i - 1][j]);
 		}
 	}
