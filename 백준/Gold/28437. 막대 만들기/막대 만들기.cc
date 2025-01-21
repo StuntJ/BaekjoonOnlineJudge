b926caf3c8ac80c65bf69652314d9cbfe89404fe
StuntJ <95505685+StuntJ@users.noreply.github.com>
2025-01-21 17:49:28 +0900

    [Gold III] Title: 막대 만들기, Time: 144 ms, Memory: 7560 KB -BaekjoonHub

--- 백준/Gold/28437. 막대 만들기/막대 만들기.cc
+++ 백준/Gold/28437. 막대 만들기/막대 만들기.cc
@@ -2,55 +2,48 @@
 #define MAX_N 100001
 
 using namespace std;
+typedef long long ll;
 
-int cache[MAX_N];
-
-int solve(int x, set<int>& a)
-{
-	if (x == 0) return 0;
-	int& ret = cache[x];
-	if (ret != -1) return ret;
-	ret = 0;
-	if (a.find(x) != a.end()) ret++;
-	if (x == 1) return ret;
-	for (int i = 1; i * i <= x; i++)
-	{
-		if (x % i == 0)
-		{
-			if (i * i == x) ret += solve(i, a);
-			else
-			{
-				ret += solve(i, a);
-				if(x/i!=x) ret += solve(x / i, a);
-			}
-		}
-	}
-	return ret;
-}
+int dp[MAX_N];
 
 int main()
 {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
-	int N;
-	cin >> N;
-	set<int> A;
-	for (int i = 0; i < N; i++)
-	{
-		int x;
-		cin >> x;
-		A.insert(x);
-	}
+    int N;
+    cin >> N;
+    set<int> A;
+    for (int i = 0; i < N; i++)
+    {
+        int x;
+        cin >> x;
+        A.insert(x);
+    }
+    int M;
+    cin >> M;
+    vector<int> B(M);
+    for (auto& i : B) cin >> i;
 
-	int M;
-	cin >> M;
-	vector<int> B(M);
-	for (auto& i : B) cin >> i;
+    if (A.find(1) != A.end()) dp[1] = 1;
+    else dp[1] = 0;
+    for (int i = 2; i < MAX_N; i++)
+    {
+        if (A.find(i) != A.end()) dp[i]++;
+        for (int j = 1; j * j <= i; j++)
+        {
+            if(i%j==0)
+            {
+                if (j * j == i) dp[i] += dp[j];
+                else
+                {
+                    dp[i] += dp[j];
+                    if (i / j != i) dp[i] += dp[i / j];
+                }
+            }
+        }
+    }
 
-	memset(cache, -1, sizeof(cache));
-	for (auto x : B)
-	{
-		cout << solve(x, A) << ' ';
-	}
+    for (auto i : B) cout << dp[i] << ' ';
+    cout << '\n';
 }
