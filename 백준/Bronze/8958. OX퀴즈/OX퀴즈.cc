5eac9e21dd6292d9be3aab9eb5d44789181929bb
StuntJ <95505685+StuntJ@users.noreply.github.com>
2025-02-02 13:14:18 +0900

    [Bronze II] Title: OX퀴즈, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        string str;
+        cin >> str;
+
+        int cnt = 0;
+        int total = 0;
+        for (int i = 0; i < str.size(); i++)
+        {
+            if (str[i] == 'O') 
+            {
+                total += cnt;
+                cnt++;
+            }
+            else
+            {
+                total += cnt;
+                cnt = 0;
+            }
+        }
+        total += cnt;
+        cout << total << '\n';
+    }
+
+}
