af658842e3e65017c96d01aa3b8e055e1c2dd20d
StuntJ <95505685+StuntJ@users.noreply.github.com>
2025-02-02 17:04:53 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 200 ms, Memory: 16680 KB -BaekjoonHub

---
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.cc
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<vector<int>>> vvvi;
+typedef vector<ll> vll;
+typedef vector<vector<ll>> vvll;
+typedef vector<vector<vector<ll>>> vvvll;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef vector<pi> vpi;
+typedef vector<vector<pi>> vvpi;
+typedef vector<vector<vector<pi>>> vvvpi;
+typedef vector<double> vd;
+typedef vector<vd> vvd;
+typedef vector<vvd> vvvd;
+typedef vector<long double> vld;
+typedef vector<vld> vvld;
+typedef vector<vvld> vvvld;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int N, M;
+    map<string, string> mp;
+    cin >> N >> M;
+
+    for (int i = 0; i < N; i++)
+    {
+        string str1, str2;
+        cin >> str1 >> str2;
+        mp[str1] = str2;
+    }
+
+    for (int i = 0; i < M; i++)
+    {
+        string str;
+        cin >> str;
+        cout << mp[str] << '\n';
+    }
+}
