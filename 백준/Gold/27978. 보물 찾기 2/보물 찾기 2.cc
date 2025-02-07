ee25a2a47f95309e080f0e7afe73fd9556931f5c
StuntJ <95505685+StuntJ@users.noreply.github.com>
2025-02-07 15:59:03 +0900

    [Gold III] Title: 보물 찾기 2, Time: 28 ms, Memory: 5716 KB -BaekjoonHub

--- 백준/Gold/27978. 보물 찾기 2/보물 찾기 2.cc
+++ 백준/Gold/27978. 보물 찾기 2/보물 찾기 2.cc
@@ -3,85 +3,81 @@
 
 using namespace std;
 
-int H,W;
-vector<vector<char>> board;
-int dx[8] = {0,0,1,-1,1,1,-1,-1};
-int dy[8] = {1,-1,0,0,1,-1,1,-1};
+int H, W;
+int dx[8] = { 0,0,1,-1,1,1,-1,-1 };
+int dy[8] = { 1,-1,0,0,1,-1,1,-1 };
+vector<vector<char>> grid;
 
 struct pq_data
 {
-    int x;
-    int y;
-    int cost;
+	int dist;
+	int x;
+	int y;
 
-    bool operator < (const pq_data &rhs) const
-    {
-        return cost>rhs.cost;
-    }
+	bool operator < (const pq_data& rhs) const
+	{
+		return dist > rhs.dist;
+	}
 };
 
 bool isInrange(int x, int y)
 {
-    return 0<=x&&x<H && 0<=y&&y<W && board[x][y]!='#';
+	return 0 <= x && x < H && 0 <= y && y < W;
 }
 
-vector<vector<int>> dijkstra(pair<int,int> src)
+vector<vector<int>> dijkstra(int stx, int sty)
 {
-    priority_queue<pq_data> pq;
-    pq.push({src.first,src.second,0});
-    vector<vector<int>> dist(H,vector<int>(W,INF));
-    dist[src.first][src.second] = 0;
-    while(!pq.empty())
-    {
-        pair<int,int> here = make_pair(pq.top().x,pq.top().y);
-        int cost = pq.top().cost;
-        pq.pop();
-        if(dist[here.first][here.second]<cost) continue;
-        for(int i=0;i<8;i++)
-        {
-            int nx = here.first + dx[i];
-            int ny = here.second + dy[i];
-            if(isInrange(nx,ny))
-            {
-                int nextDist = cost;
-                if(dy[i]!=1) nextDist++;
-                if(dist[nx][ny]>nextDist)
-                {
-                    dist[nx][ny] = nextDist;
-                    pq.push({nx,ny,nextDist});
-                }
-            }
-        }
-    }
-    return dist;
+	vector<vector<int>> dist(H, vector<int>(W, INF));
+	dist[stx][sty] = 0;
+	priority_queue<pq_data> pq;
+	pq.push({ 0,stx,sty });
+	
+	while (!pq.empty())
+	{
+		pq_data here = pq.top(); pq.pop();
+
+		if (here.dist > dist[here.x][here.y]) continue;
+
+		for (int i = 0; i < 8; i++)
+		{
+			int nx = here.x + dx[i];
+			int ny = here.y + dy[i];
+			int nextDist = here.dist + (dy[i] != 1);
+
+			if (!isInrange(nx, ny) || grid[nx][ny] == '#') continue;
+
+			if (dist[nx][ny] > nextDist)
+			{
+				dist[nx][ny] = nextDist;
+				pq.push({ nextDist,nx,ny });
+			}
+		}
+	}
+	return dist;
 }
 
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+	ios::sync_with_stdio(0);
+	cin.tie(0);
 
-    cin>>H>>W;
+	cin >> H >> W;
+	
+	grid = vector<vector<char>>(H, vector<char>(W));
+	
+	int stx, sty, ndx, ndy;
+	for (int i = 0; i < H; i++)
+	{
+		for (int j = 0; j < W; j++)
+		{
+			cin >> grid[i][j];
+			if (grid[i][j] == 'K') stx = i, sty = j;
+			else if (grid[i][j] == '*') ndx = i, ndy = j;
+		}
+	}
 
-    board.resize(H,vector<char>(W));
+	vector<vector<int>> dist = dijkstra(stx, sty);
 
-    pair<int,int> src;
-    pair<int,int> tresure;
-
-    for(int i=0;i<H;i++)
-    {
-        for(int j=0;j<W;j++)
-        {
-            cin>>board[i][j];
-            if(board[i][j]=='K') src = make_pair(i,j);
-            if(board[i][j]=='*') tresure = make_pair(i,j);
-        }
-    }
-
-    vector<vector<int>> dist = dijkstra(src);
-
-    int ans = dist[tresure.first][tresure.second];
-    if(ans==INF) cout<<-1;
-    else cout<<dist[tresure.first][tresure.second];
+	if (dist[ndx][ndy] >= INF) cout << -1 << '\n';
+	else cout << dist[ndx][ndy] << '\n';
 }
