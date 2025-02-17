4474c6b0301aa7c5050f2c0639521f9321f7180b
StuntJ <95505685+StuntJ@users.noreply.github.com>
2025-02-17 18:09:29 +0900

    [Gold III] Title: 텀 프로젝트, Time: 1084 ms, Memory: 20444 KB -BaekjoonHub

---
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.cc
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<vector<int>>> vvvi;
+typedef vector<ll> vll;
+typedef vector<vector<ll>> vvll;
+typedef vector<vector<vector<ll>>> vvvll;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef vector<pi> vpi;
+typedef vector<vector<pi>> vvpi;
+typedef vector<vector<vector<pi>>> vvvpi;
+typedef vector<double> vd;
+typedef vector<vd> vvd;
+typedef vector<vvd> vvvd;
+typedef vector<long double> vld;
+typedef vector<vld> vvld;
+typedef vector<vvld> vvvld;
+typedef vector<bool> vb;
+typedef vector<vector<bool>> vvb;
+typedef vector<vector<vector<bool>>> vvvb;
+typedef vector<char> vc;
+typedef vector<vc> vvc;
+typedef vector<vvc> vvvc;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int N;
+        cin >> N;
+
+        vb visited(N + 1);
+        vvi adj(N + 1);
+        vb notfin(N + 1);
+        for (int i = 1; i <= N; i++)
+        {
+            int x;
+            cin >> x;
+
+            adj[i].push_back(x);
+        }
+
+        stack<int> st;
+        vb chk(N + 1);
+
+        auto dfs = [&](auto& dfs, int here)->void
+            {
+                notfin[here] = true;
+                visited[here] = true;
+                st.push(here);
+
+                for (auto next : adj[here])
+                {
+                    if (notfin[next])
+                    {
+                        while (true)
+                        {
+                            int k = st.top(); st.pop();
+                            chk[k] = true;
+                            if (k == next) break;
+                        }
+                    }
+                    if (!visited[next])
+                        dfs(dfs, next);
+                }
+
+                notfin[here] = false;
+            };
+
+        auto dfs_all = [&]()->void
+            {
+                for (int i = 1; i <= N; i++)
+                {
+                    if (!visited[i])
+                        dfs(dfs, i);
+                }
+            };
+
+        dfs_all();
+        int cnt = 0;
+        for (int i = 1; i <= N; i++)
+            if (!chk[i]) cnt++;
+        cout << cnt << '\n';
+    }
+}
