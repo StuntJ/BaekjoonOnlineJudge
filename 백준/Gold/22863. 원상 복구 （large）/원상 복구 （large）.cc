eab4a16ec1813be8246b2e54c51ff2e5fa683e61
StuntJ <95505685+StuntJ@users.noreply.github.com>
2025-02-19 21:15:43 +0900

    [Gold II] Title: 원상 복구 (large), Time: 2468 ms, Memory: 205356 KB -BaekjoonHub

---
+++ 백준/Gold/22863. 원상 복구 （large）/원상 복구 （large）.cc
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<vector<int>>> vvvi;
+typedef vector<ll> vll;
+typedef vector<vector<ll>> vvll;
+typedef vector<vector<vector<ll>>> vvvll;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef vector<pi> vpi;
+typedef vector<vector<pi>> vvpi;
+typedef vector<vector<vector<pi>>> vvvpi;
+typedef vector<double> vd;
+typedef vector<vd> vvd;
+typedef vector<vvd> vvvd;
+typedef vector<long double> vld;
+typedef vector<vld> vvld;
+typedef vector<vvld> vvvld;
+typedef vector<bool> vb;
+typedef vector<vector<bool>> vvb;
+typedef vector<vector<vector<bool>>> vvvb;
+typedef vector<char> vc;
+typedef vector<vc> vvc;
+typedef vector<vvc> vvvc;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    const int lim = 50;
+
+    ll N, K;
+    cin >> N >> K;
+
+    vi v(N);
+    for (auto& i : v) cin >> i;
+
+    vvi dp(lim, vi(N+1));
+    for (int i = 1; i <= N; i++)
+        cin >> dp[0][i];
+
+    for (int i = 1; i < lim; i++)
+        for (int j = 1; j <= N; j++)
+            dp[i][j] = dp[i - 1][dp[i - 1][j]];
+
+    vi ans(N + 1);
+    for(int i=0;i<N;i++)
+    {
+        ll t = K;
+        int here = i+1;
+        for (int j = lim; j >= 0; j--)
+        {
+            if (t >= (1LL << j)) 
+            {
+                here = dp[j][here];
+                t -= (1LL << j);
+            }
+        }
+        ans[here] = v[i];
+    }
+    for (int i = 1; i <= N; i++)
+        cout << ans[i] << ' ';
+}
