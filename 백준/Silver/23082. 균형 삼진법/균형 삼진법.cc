830ff57663e631a9fa2508c47b7afcda26f21825
StuntJ <95505685+StuntJ@users.noreply.github.com>
2025-02-27 20:27:50 +0900

    [Silver I] Title: 균형 삼진법, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/23082. 균형 삼진법/균형 삼진법.cc
+++ 백준/Silver/23082. 균형 삼진법/균형 삼진법.cc
@@ -2,15 +2,6 @@
 
 using namespace std;
 
-int fastPow(int n, int p)
-{
-	if (p == 0) return 1;
-	int temp = fastPow(n, p / 2);
-	int ret = temp * temp;
-	if (p & 1) return ret * n;
-	return ret;
-}
-
 int main()
 {
 	ios::sync_with_stdio(0);
@@ -19,40 +10,30 @@ int main()
 	int N;
 	cin >> N;
 
-	if (N == 0)
+	if (!N)
 	{
 		cout << 0;
 		return 0;
 	}
-
 	bool rev = false;
 	if (N < 0) rev = true, N *= -1;
-	int lim = (int)floor(log10(N) / log10(3));
-	vector<int> ans;
 
-	for (int i = lim; i >= 0; i--)
+	string S;
+
+	while (N > 0)
 	{
-		int bit = fastPow(3, i);
-		ans.push_back(N / bit);
-		N %= bit;
+		if (N % 3 == 2) S += 'T', N++;
+		else S += '0' + N % 3, N -= N % 3;
+		N /= 3;
 	}
-
-	reverse(ans.begin(), ans.end());
-	for (int i = 0; i < ans.size(); i++)
+	reverse(S.begin(), S.end());
+	if (rev)
 	{
-		if (ans[i] >= 2) 
+		for (auto& ch : S)
 		{
-			ans[i] -= 3;
-			if (i + 1 < ans.size()) ans[i + 1]++;
-			else ans.push_back(1);
+			if (ch == 'T') ch = '1';
+			else if (ch == '1') ch = 'T';
 		}
 	}
-	reverse(ans.begin(), ans.end());
-	if (rev) for (auto& i : ans) i *= -1;
-
-	for (auto i : ans)
-	{
-		if (i == -1) cout << 'T';
-		else cout << i;
-	}
+	cout << S;
 }
