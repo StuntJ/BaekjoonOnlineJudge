6fd4ece4dbe94e24eb938182b59fe89df39034be
StuntJ <95505685+StuntJ@users.noreply.github.com>
2025-03-23 14:37:57 +0900

    [Platinum V] Title: 가장 긴 팰린드롬 부분 문자열, Time: 4 ms, Memory: 3500 KB -BaekjoonHub

---
+++ 백준/Platinum/13275. 가장 긴 팰린드롬 부분 문자열/가장 긴 팰린드롬 부분 문자열.cc
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+//매내처 알고리즘은 팰린드롬의 대칭성을 이용하는 알고리즘이다.
+// 함수의 리턴값은 i로 부터 팰린드롬의 최대 반지름을 저장하는 배열이다.
+// 짝수 팰린드롬을 구하기 위해, "ABCDEGDSE" -> "@A@B@C@D@E@G@D@S@E@" 꼴로 만들어 계산한다. 이때 계산되는 값이 팰린드롬의 길이가 된다.
+//https://ialy1595.github.io/post/manacher/를 참조한다.
+
+vector<int> manacher(string str)
+{
+	int p=-1 , r = -1;
+	int len = str.size();
+	vector<int> res(len);
+
+	for (int i = 0; i < len; i++)
+	{
+		if (i > r) //끝 범위보다 크면 대칭성을 사용할 수 없다.
+		{
+			p = r = i;
+			while (r < len && 0 <= 2 * p - r && str[r] == str[2 * p - r]) r++;
+			r--;
+			res[i] = r - p;
+			
+		}
+		else
+		{
+			int j = 2 * p - i;
+			if (res[j] < r - i)
+				res[i] = res[j];
+			else if (res[j] > r - i)
+				res[i] = r - i;
+			else
+			{
+				p = i;
+				while(r < len && 0 <= 2 * p - r && str[r] == str[2 * p - r]) r++;
+				r--;
+				res[i] = r - p;
+				
+			}
+		}
+	}
+	return res;
+}
+
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	string str;
+	cin >> str;
+
+	string adv = "@";
+	for (auto i : str)
+	{
+		adv.push_back(i);
+		adv.push_back('@');
+	}
+
+	vector<int> res = manacher(adv);
+	/*for (auto i : res) cout << i << ' ';
+	cout << '\n';*/
+
+	int ans = 0;
+	for (auto i : res)
+		ans = max(ans, i);
+
+	cout << ans;
+}
