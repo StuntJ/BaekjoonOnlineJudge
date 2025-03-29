c505c1224b81aa3b548a4782fc6730ff0cd3ce37
StuntJ <95505685+StuntJ@users.noreply.github.com>
2025-03-29 23:17:12 +0900

    [Platinum IV] Title: 불 끄기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Platinum/14939. 불 끄기/불 끄기.cc
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	const int N = 10;
+	vector<int> v(N);
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			int k = 1 << j;
+			char x;
+			cin >> x;
+			if (x == 'O') v[i] |= k;
+		}
+	}
+	//for (int i = 0; i < N; i++) cout << bitset<10>(v[i]) << '\n';
+	
+
+	int ans = 987654321;
+	for (int i = 0; i < (1 << N); i++)
+	{
+		int cnt = 0;
+		vector<int> cp = v;
+		for (int j = 0; j < N; j++)
+		{
+			if ((1 << j) & i)
+			{
+				cnt++;
+				cp[0] ^= (1 << j);
+				cp[1] ^= (1 << j);
+				if (j > 0) cp[0] ^= (1 << (j - 1));
+				if (j < N - 1) cp[0] ^= (1 << (j + 1));
+			}
+		}
+		//if(i==0) cout << bitset<10>(cp[0]) << '\n';
+		for (int j = 1; j < N; j++)
+		{
+			for (int k = 0; k < N; k++)
+			{
+				if ((1 << k) & cp[j - 1])
+				{
+					cnt++;
+					cp[j] ^= (1 << k);
+					if (j < N - 1) cp[j + 1] ^= (1 << k);
+					if (k > 0) cp[j] ^= (1 << (k - 1));
+					if (k < N - 1) cp[j] ^= (1 << (k + 1));
+				}
+			}
+			if (j == N - 1)
+			{
+				if (cp[j] == 0) ans = min(ans, cnt);
+			}
+			//if(i==0) cout << bitset<10>(cp[j]) << '\n';
+		}
+	}
+	cout << (ans == 987654321 ? -1 : ans);
+}
