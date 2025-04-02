f7cc81462911eb49ad2171608f7bee5c19b080ce
StuntJ <95505685+StuntJ@users.noreply.github.com>
2025-04-02 23:56:47 +0900

    [Diamond IV] Title: 라면 사기 (Large), Time: 96 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Diamond/18186. 라면 사기 （Large）/라면 사기 （Large）.cc
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<vector<int>>> vvvi;
+typedef vector<ll> vll;
+typedef vector<vector<ll>> vvll;
+typedef vector<vector<vector<ll>>> vvvll;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef vector<pi> vpi;
+typedef vector<vector<pi>> vvpi;
+typedef vector<vector<vector<pi>>> vvvpi;
+typedef vector<double> vd;
+typedef vector<vd> vvd;
+typedef vector<vvd> vvvd;
+typedef vector<long double> vld;
+typedef vector<vld> vvld;
+typedef vector<vvld> vvvld;
+typedef vector<bool> vb;
+typedef vector<vector<bool>> vvb;
+typedef vector<vector<vector<bool>>> vvvb;
+typedef vector<char> vc;
+typedef vector<vc> vvc;
+typedef vector<vvc> vvvc;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int N, B, C;
+    cin >> N >> B >> C;
+    ll ans = 0;
+    
+    vi v(N);
+    for (auto& i : v) cin >> i;
+    if (B <= C)
+    {
+        cout << accumulate(v.begin(), v.end(), 0LL) * B;
+        return 0;
+    }
+
+    auto buy1 = [&](int st)
+        {
+            ll mul = v[st];
+            v[st] = 0;
+            return mul * B;
+        };
+
+    auto buy2 = [&](int st)
+        {
+            ll t = min(v[st], v[st + 1]);
+            v[st] -= t;
+            v[st + 1] -= t;
+            return t * (B+C);
+        };
+
+    auto buy3 = [&](int st)
+        {
+            ll t = min(v[st], min(v[st + 1], v[st + 2]));
+            v[st] -= t;
+            v[st + 1] -= t;
+            v[st + 2] -= t;
+            return t * (B+2*C);
+        };
+
+    for (int i = 0; i < N - 2; i++)
+    {
+        if (v[i + 1] > v[i + 2])
+        {
+            ll diff = v[i + 1] - v[i + 2];
+            diff = min(diff, (ll)v[i]);
+            v[i] -= diff;
+            v[i + 1] -= diff;
+            ans += diff * (B+C);
+            ans += buy3(i);
+            ans += buy1(i);
+        }
+        else
+        {
+            ans += buy3(i);
+            ans += buy2(i);
+            ans += buy1(i);
+        }
+    }
+    ans += buy2(N - 2);
+    ans += buy1(N - 2);
+    ans += buy1(N - 1);
+    cout << ans;
+}
