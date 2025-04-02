2fd58ebe5ad615453d412794a8f237d7729c2bc3
StuntJ <95505685+StuntJ@users.noreply.github.com>
2025-04-02 23:32:12 +0900

    [Platinum V] Title: 팰린드롬??, Time: 380 ms, Memory: 25972 KB -BaekjoonHub

---
+++ 백준/Platinum/11046. 팰린드롬？？/팰린드롬？？.cc
@@ -0,0 +1,106 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<vector<int>>> vvvi;
+typedef vector<ll> vll;
+typedef vector<vector<ll>> vvll;
+typedef vector<vector<vector<ll>>> vvvll;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef vector<pi> vpi;
+typedef vector<vector<pi>> vvpi;
+typedef vector<vector<vector<pi>>> vvvpi;
+typedef vector<double> vd;
+typedef vector<vd> vvd;
+typedef vector<vvd> vvvd;
+typedef vector<long double> vld;
+typedef vector<vld> vvld;
+typedef vector<vvld> vvvld;
+typedef vector<bool> vb;
+typedef vector<vector<bool>> vvb;
+typedef vector<vector<vector<bool>>> vvvb;
+typedef vector<char> vc;
+typedef vector<vc> vvc;
+typedef vector<vvc> vvvc;
+
+//매내처 알고리즘은 팰린드롬의 대칭성을 이용하는 알고리즘이다.
+// 함수의 리턴값은 i로 부터 팰린드롬의 최대 반지름을 저장하는 배열이다.
+// 짝수 팰린드롬을 구하기 위해, "ABCDEGDSE" -> "@A@B@C@D@E@G@D@S@E@" 꼴로 만들어 계산한다. 이때 계산되는 값이 팰린드롬의 길이가 된다.
+//https://ialy1595.github.io/post/manacher/를 참조한다.
+
+vector<int> manacher(vector<int> str)
+{
+	int p = -1, r = -1;
+	int len = str.size();
+	vector<int> res(len);
+
+	for (int i = 0; i < len; i++)
+	{
+		if (i > r) //끝 범위보다 크면 대칭성을 사용할 수 없다.
+		{
+			p = r = i;
+			while (r < len && 0 <= 2 * p - r && str[r] == str[2 * p - r]) r++;
+			r--;
+			res[i] = r - p;
+
+		}
+		else
+		{
+			int j = 2 * p - i;
+			if (res[j] < r - i)
+				res[i] = res[j];
+			else if (res[j] > r - i)
+				res[i] = r - i;
+			else
+			{
+				p = i;
+				while (r < len && 0 <= 2 * p - r && str[r] == str[2 * p - r]) r++;
+				r--;
+				res[i] = r - p;
+
+			}
+		}
+	}
+	return res;
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+	int N;
+	cin >> N;
+
+	vector<int> str = { -1 };
+	for (int i = 0; i < N; i++)
+	{
+		int x;
+		cin >> x;
+		str.push_back(x);
+		str.push_back(-1);
+	}
+
+	vi v = manacher(str);
+
+	//for (auto i : v) cout << i << ' ';
+	//cout << '\n';
+
+	int M;
+	cin >> M;
+
+	while (M--)
+	{
+		int s, e;
+		cin >> s >> e;
+		s = s * 2 - 1;
+		e = e * 2 - 1;
+		int mid = (s + e) / 2;
+		int val = v[mid];
+		if (mid + val >= e) cout << 1 << '\n';
+		else cout << 0 << '\n';
+	}
+}
