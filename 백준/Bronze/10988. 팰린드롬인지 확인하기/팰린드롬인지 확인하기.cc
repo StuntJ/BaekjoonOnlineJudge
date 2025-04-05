98f85e37c33d056fc547eda33c5d51ece7eb4f02
StuntJ <95505685+StuntJ@users.noreply.github.com>
2025-04-05 20:20:37 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<vector<int>>> vvvi;
+typedef vector<ll> vll;
+typedef vector<vector<ll>> vvll;
+typedef vector<vector<vector<ll>>> vvvll;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef vector<pi> vpi;
+typedef vector<vector<pi>> vvpi;
+typedef vector<vector<vector<pi>>> vvvpi;
+typedef vector<double> vd;
+typedef vector<vd> vvd;
+typedef vector<vvd> vvvd;
+typedef vector<long double> vld;
+typedef vector<vld> vvld;
+typedef vector<vvld> vvvld;
+typedef vector<bool> vb;
+typedef vector<vector<bool>> vvb;
+typedef vector<vector<vector<bool>>> vvvb;
+typedef vector<char> vc;
+typedef vector<vc> vvc;
+typedef vector<vvc> vvvc;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    auto isPal = [&](string str)
+        {
+            int n = str.size();
+            for (int i = 0; i < n; i++)
+                if (str[i] != str[n - i - 1]) return false;
+            return true;
+        };
+
+    string str;
+    cin >> str;
+    cout << isPal(str);
+}
