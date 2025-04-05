4c5a1fd0f9f58ab4f8e7d133f6e9cbbe2f070d63
StuntJ <95505685+StuntJ@users.noreply.github.com>
2025-04-05 20:31:17 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.cc
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<vector<int>>> vvvi;
+typedef vector<ll> vll;
+typedef vector<vector<ll>> vvll;
+typedef vector<vector<vector<ll>>> vvvll;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef vector<pi> vpi;
+typedef vector<vector<pi>> vvpi;
+typedef vector<vector<vector<pi>>> vvvpi;
+typedef vector<double> vd;
+typedef vector<vd> vvd;
+typedef vector<vvd> vvvd;
+typedef vector<long double> vld;
+typedef vector<vld> vvld;
+typedef vector<vvld> vvvld;
+typedef vector<bool> vb;
+typedef vector<vector<bool>> vvb;
+typedef vector<vector<vector<bool>>> vvvb;
+typedef vector<char> vc;
+typedef vector<vc> vvc;
+typedef vector<vvc> vvvc;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int arr[6] = { 1,1,2,2,2,8 };
+    int v[6];
+    for (int i = 0; i < 6; i++)
+        cin >> v[i];
+
+    for (int i = 0; i < 6; i++)
+        cout << arr[i] - v[i] << ' ';
+}
