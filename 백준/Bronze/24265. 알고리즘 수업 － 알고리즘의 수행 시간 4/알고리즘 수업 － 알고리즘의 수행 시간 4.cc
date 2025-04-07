c92df3d07b5a8e6f9e424386ebbda164cae9f2ec
StuntJ <95505685+StuntJ@users.noreply.github.com>
2025-04-07 21:51:28 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    typedef long long ll;
+    ll N;
+    cin>>N;
+    cout<<N*(N-1)/2<<'\n'<<2;
+}
