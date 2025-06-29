dafb5e75b6156368d2572d71d68d71f61312424c
StuntJ <95505685+StuntJ@users.noreply.github.com>
2025-06-29 10:14:22 +0900

    [Gold III] Title: 출퇴근, Time: 908 ms, Memory: 803744 KB, Score: 140 point -BaekjoonHub

--- 백준/Gold/20313. 출퇴근/출퇴근.cc
+++ 백준/Gold/20313. 출퇴근/출퇴근.cc
@@ -16,51 +16,67 @@ int main() {
 	int N, M, A, B;
 	cin >> N >> M >> A >> B;
 	vector<tuple<ll,ll,ll>> R(M);
-	vector<vector<ll>> adj(N + 1,vector<ll>(N+1,-1));
+
 	for (int i = 0; i < M; i++) {
 		ll a, b, c;
 		cin >> a >> b >> c;
 		R[i] = { a,b,c };
-		adj[a][b] = c;
-		adj[b][a] = c;
 	}
-	
-	const ll INF = 1e15;
-	auto dijkstra = [&]()->ll {
-		vector<ll> dist(N + 1, INF);
-		priority_queue<pair<ll,int>> pq;
-		dist[A] = 0;
-		pq.push({ 0,A });
-		while (!pq.empty()) {
-			auto [hereDist, here] = pq.top();
-			hereDist = -hereDist;
-			pq.pop();
-			
-			if (hereDist > dist[here]) continue;
-			for (int next = 1; next <= N; next++) {
-				if (adj[here][next] == -1) continue;
-				ll nextDist = hereDist + adj[here][next];
-				if (nextDist < dist[next]) {
-					dist[next] = nextDist;
-					pq.push({ -nextDist,next });
-				}
-			}
-		}
-		return dist[B];
-	};
 
-	ll ans = dijkstra();
 	int K;
 	cin >> K;
-	for (int i = 0; i < K; i++) {
-		for(int j=0;j<M;j++){
+	vector<vector<vector<ll>>> adj(K + 1, vector<vector<ll>>(N + 1, vector<ll>(N + 1,-1)));
+	for (int i = 0; i < M; i++) {
+		auto [x, y, t] = R[i];
+		adj[0][x][y] = t;
+		adj[0][y][x] = t;
+	}
+
+	for (int i = 1; i <= K; i++) {
+		for (int j = 0; j < M; j++) {
 			auto [x, y, t] = R[j];
 			ll nt;
 			cin >> nt;
-			adj[x][y] = nt;
-			adj[y][x] = nt;
+			adj[i][x][y] = nt;
+			adj[i][y][x] = nt;
 		}
-		ans = min(dijkstra(), ans);
 	}
+
+	const ll INF = 1e15;
+	auto dijkstra = [&]()->ll {
+		vector<vector<ll>> dist(K+1,vector<ll>(N+1,INF));
+		priority_queue<tuple<ll,int,int>> pq;
+		dist[0][A] = 0;
+		pq.push({ 0,A,0 });
+		while (!pq.empty()) {
+			auto [hereDist, here, hidx] = pq.top();
+			hereDist = -hereDist;
+			pq.pop();
+			
+			if (hereDist > dist[hidx][here]) continue;
+			for (int next = 1; next <= N; next++) {
+				if (adj[hidx][here][next] == -1) continue;
+				ll nextDist = hereDist + adj[hidx][here][next];
+				if (nextDist < dist[hidx][next]) {
+					dist[hidx][next] = nextDist;
+					pq.push({ -nextDist,next,hidx });
+				}
+			}
+
+			for (int i = hidx+1; i <= K; i++) {
+				if(dist[i][here]>hereDist){
+					dist[i][here] = hereDist;
+					pq.push({ -hereDist,here,i });
+				}
+			}
+		}
+		ll ret = INF;
+		for (int i = 0; i <= K; i++) ret = min(ret, dist[i][B]);
+		return ret;
+	};
+
+	ll ans = INF;
+	
+	ans = min(dijkstra(), ans);
 	cout << ans;
 }
