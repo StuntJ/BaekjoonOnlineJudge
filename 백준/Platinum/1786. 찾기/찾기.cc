68ecfec422101c676cb70f5b1a11fe2560ae2752
StuntJ <95505685+StuntJ@users.noreply.github.com>
2025-07-21 23:40:06 +0900

    [Platinum V] Title: 찾기, Time: 48 ms, Memory: 9432 KB -BaekjoonHub

---
+++ 백준/Platinum/1786. 찾기/찾기.cc
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+vector<int> getPartialMatch(const string &N)
+{
+    int m = N.size();
+    vector<int> pi(m,0);
+
+    int begin = 1, matched = 0;
+
+    while(begin+matched < m){
+        if(N[begin + matched] == N[matched]){
+            ++matched;
+            pi[begin+matched-1] = matched;
+        }
+        else{
+            if(matched == 0)
+                ++begin;
+            else{
+                begin += matched - pi[matched-1];
+                matched = pi[matched-1];
+            }
+        }
+    }
+
+    return pi;
+}
+
+vector<int> kmpSearch(const string &H, const string &N){
+    int n = H.size(), m = N.size();
+    vector<int> ret;
+    vector<int> pi = getPartialMatch(N);
+    int begin = 0, matched = 0;
+    while(begin <= n-m){
+        if(matched < m && H[begin + matched] == N[matched]){
+            ++matched;
+            if(matched == m) ret.emplace_back(begin);
+        }
+        else{
+            if(matched == 0)
+                ++begin;
+            else{
+                begin += matched - pi[matched-1];
+                matched = pi[matched-1];
+            }
+        }
+    }
+    return ret;
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    string H,N;
+    getline(cin,H);
+    getline(cin,N);
+
+    vector<int> res = kmpSearch(H,N);
+    cout<<res.size()<<'\n';
+    for(auto i : res) cout<<i+1<<' ';
+}
