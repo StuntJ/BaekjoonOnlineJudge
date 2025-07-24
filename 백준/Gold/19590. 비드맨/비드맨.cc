7557b3a6b1b51baa1ab34db45c35398892b6cfd3
StuntJ <95505685+StuntJ@users.noreply.github.com>
2025-07-24 22:51:14 +0900

    [Gold II] Title: 비드맨, Time: 16 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Gold/19590. 비드맨/비드맨.cc
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<vector<int>>> vvvi;
+typedef vector<ll> vll;
+typedef vector<vector<ll>> vvll;
+typedef vector<vector<vector<ll>>> vvvll;
+typedef pair<int,int> pi;
+typedef pair<ll,ll> pl;
+typedef vector<pi> vpi;
+typedef vector<vector<pi>> vvpi;
+typedef vector<vector<vector<pi>>> vvvpi;
+typedef vector<double> vd;
+typedef vector<vd> vvd;
+typedef vector<vvd> vvvd;
+typedef vector<long double> vld;
+typedef vector<vld> vvld;
+typedef vector<vvld> vvvld;
+typedef vector<bool> vb;
+typedef vector<vector<bool>> vvb;
+typedef vector<vector<vector<bool>>> vvvb;
+typedef vector<char> vc;
+typedef vector<vc> vvc;
+typedef vector<vvc> vvvc;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int N;
+    cin>>N;
+    vector<int> v(N);
+    for(auto &i : v) cin>>i;
+    int x = *max_element(v.begin(),v.end());
+    ll sum = accumulate(v.begin(),v.end(),0LL)-x;
+    if(x>=sum) cout<<x-sum;
+    else cout<<(((sum-x)&1) ? 1 : 0);
+}
