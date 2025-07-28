109ea88b59be5216dc6f1f8c743ac242bc9581ad
StuntJ <95505685+StuntJ@users.noreply.github.com>
2025-07-28 17:32:49 +0900

    [Gold III] Title: 난개발, Time: 148 ms, Memory: 9076 KB -BaekjoonHub

--- 백준/Gold/19584. 난개발/난개발.cc
+++ 백준/Gold/19584. 난개발/난개발.cc
@@ -2,29 +2,6 @@
 
 using namespace std;
 typedef long long ll;
-typedef vector<int> vi;
-typedef vector<vector<int>> vvi;
-typedef vector<vector<vector<int>>> vvvi;
-typedef vector<ll> vll;
-typedef vector<vector<ll>> vvll;
-typedef vector<vector<vector<ll>>> vvvll;
-typedef pair<int,int> pi;
-typedef pair<ll,ll> pl;
-typedef vector<pi> vpi;
-typedef vector<vector<pi>> vvpi;
-typedef vector<vector<vector<pi>>> vvvpi;
-typedef vector<double> vd;
-typedef vector<vd> vvd;
-typedef vector<vvd> vvvd;
-typedef vector<long double> vld;
-typedef vector<vld> vvld;
-typedef vector<vvld> vvvld;
-typedef vector<bool> vb;
-typedef vector<vector<bool>> vvb;
-typedef vector<vector<vector<bool>>> vvvb;
-typedef vector<char> vc;
-typedef vector<vc> vvc;
-typedef vector<vvc> vvvc;
 
 int main()
 {
