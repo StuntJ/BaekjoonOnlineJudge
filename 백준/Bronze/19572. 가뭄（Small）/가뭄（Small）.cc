5ddb380f79d3469e459d7422e6506eb529df8222
StuntJ <95505685+StuntJ@users.noreply.github.com>
2025-07-30 14:52:01 +0900

    [Bronze III] Title: 가뭄(Small), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/19572. 가뭄（Small）/가뭄（Small）.cc
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<vector<int>>> vvvi;
+typedef vector<ll> vll;
+typedef vector<vector<ll>> vvll;
+typedef vector<vector<vector<ll>>> vvvll;
+typedef pair<int,int> pi;
+typedef pair<ll,ll> pl;
+typedef vector<pi> vpi;
+typedef vector<vector<pi>> vvpi;
+typedef vector<vector<vector<pi>>> vvvpi;
+typedef vector<double> vd;
+typedef vector<vd> vvd;
+typedef vector<vvd> vvvd;
+typedef vector<long double> vld;
+typedef vector<vld> vvld;
+typedef vector<vvld> vvvld;
+typedef vector<bool> vb;
+typedef vector<vector<bool>> vvb;
+typedef vector<vector<vector<bool>>> vvvb;
+typedef vector<char> vc;
+typedef vector<vc> vvc;
+typedef vector<vvc> vvvc;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int al,be,ga;
+    cin>>al>>be>>ga;
+    cout<<fixed;
+    cout.precision(1);
+
+    double sum = (al+be+ga)/2.0;
+    double a = sum-ga;
+    double b = sum-be;
+    double c = sum-al;
+    if(a>0&&b>0&&c>0){
+        cout<<1<<'\n';
+        cout<<a<<' '<<b<<' '<<c;
+    }
+        
+    else cout<<-1;
+}
