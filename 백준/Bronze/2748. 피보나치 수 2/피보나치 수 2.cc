205921ff38c13051e6fe27c53699645c53a80acc
StuntJ <95505685+StuntJ@users.noreply.github.com>
2025-08-07 00:27:21 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.cc
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<vector<int>>> vvvi;
+typedef vector<ll> vll;
+typedef vector<vector<ll>> vvll;
+typedef vector<vector<vector<ll>>> vvvll;
+typedef pair<int,int> pi;
+typedef pair<ll,ll> pl;
+typedef vector<pi> vpi;
+typedef vector<vector<pi>> vvpi;
+typedef vector<vector<vector<pi>>> vvvpi;
+typedef vector<double> vd;
+typedef vector<vd> vvd;
+typedef vector<vvd> vvvd;
+typedef vector<long double> vld;
+typedef vector<vld> vvld;
+typedef vector<vvld> vvvld;
+typedef vector<bool> vb;
+typedef vector<vector<bool>> vvb;
+typedef vector<vector<vector<bool>>> vvvb;
+typedef vector<char> vc;
+typedef vector<vc> vvc;
+typedef vector<vvc> vvvc;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    vll fibo(100);
+    fibo[1] = fibo[2] = 1;
+    for(int i=2;i<100;i++) fibo[i] = fibo[i-1] + fibo[i-2];
+    int N;
+    cin>>N;
+    cout<<fibo[N];
+}
