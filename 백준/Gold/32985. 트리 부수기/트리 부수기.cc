4120e4092e3a355d4b81f0137643a6d0909cc580
StuntJ <95505685+StuntJ@users.noreply.github.com>
2025-08-06 21:50:36 +0900

    [Gold III] Title: 트리 부수기, Time: 104 ms, Memory: 19068 KB -BaekjoonHub

--- 백준/Gold/32985. 트리 부수기/트리 부수기.cc
+++ 백준/Gold/32985. 트리 부수기/트리 부수기.cc
@@ -1,50 +1,60 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
-vector<vector<int>> adj;
-vector<bool> visited;
-vector<int> depth;
-int V;
-
-void dfs(int src, int d)
-{
-	visited[src] = true;
-	depth[src] = d;
-	for (auto next : adj[src])
-	{
-		if (visited[next]) continue;
-		dfs(next, d + 1);
-	}
-}
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<vector<int>>> vvvi;
+typedef vector<ll> vll;
+typedef vector<vector<ll>> vvll;
+typedef vector<vector<vector<ll>>> vvvll;
+typedef pair<int,int> pi;
+typedef pair<ll,ll> pl;
+typedef vector<pi> vpi;
+typedef vector<vector<pi>> vvpi;
+typedef vector<vector<vector<pi>>> vvvpi;
+typedef vector<double> vd;
+typedef vector<vd> vvd;
+typedef vector<vvd> vvvd;
+typedef vector<long double> vld;
+typedef vector<vld> vvld;
+typedef vector<vvld> vvvld;
+typedef vector<bool> vb;
+typedef vector<vector<bool>> vvb;
+typedef vector<vector<vector<bool>>> vvvb;
+typedef vector<char> vc;
+typedef vector<vc> vvc;
+typedef vector<vvc> vvvc;
 
 int main()
 {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
-	cin >> V;
+    int N;
+    cin>>N;
 
-	adj.resize(V);
-	visited.assign(V, false);
-	depth.assign(V, 0);
+    vvi adj(N);
+    for(int i=0;i<N-1;i++){
+        int u,v;
+        cin>>u>>v;
 
-	for (int i = 0; i < V-1; i++)
-	{
-		int u, v;
-		cin >> u >> v;
+        adj[u].emplace_back(v);
+        adj[v].emplace_back(u);
+    }
 
-		adj[u].push_back(v);
-		adj[v].push_back(u);
-	}
+    vi depth(N,-1);
 
-	dfs(0, 0);
-	/*for (auto k : depth) cout << k << ' ';
-	cout << '\n';*/
-
-	for (int i = V - 1; i >= 1; i--)
-	{
-		if ((V - 1 - (depth[i])) % 2 == 0) cout << 0;
-		else cout << 1;
-	}
+    auto dfs = [&](auto &dfs, int here, int d)->void{
+        depth[here] = d;
+        for(auto next : adj[here]){
+            if(depth[next]==-1){
+                dfs(dfs,next,d+1);
+            }
+        }
+    };
+    dfs(dfs,0,0);
+    for(int i=N-1;i>0;i--){
+        cout<<(N-1-depth[i])%2;
+    }
 }
