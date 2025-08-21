9202325f6030cee3a8dfa04d320989a293eb4fb7
StuntJ <95505685+StuntJ@users.noreply.github.com>
2025-08-22 05:41:46 +0900

    [Platinum V] Title: 학식 뭐 먹지, Time: 8 ms, Memory: 2964 KB -BaekjoonHub

--- 백준/Platinum/33525. 학식 뭐 먹지/학식 뭐 먹지.cc
+++ 백준/Platinum/33525. 학식 뭐 먹지/학식 뭐 먹지.cc
@@ -1,43 +1,39 @@
 #include <bits/stdc++.h>
+
 using namespace std;
-using ll = long long;
+typedef long long ll;
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
-    int N, M;
-    cin >> N >> M;
+    ll N,M;
+    cin>>N>>M;
+
     vector<pair<ll,ll>> menu(M);
-    for (auto &p : menu) cin >> p.first >> p.second;
-    sort(menu.begin(), menu.end()); // 가격 오름차순
+    for(auto &i : menu) cin>>i.first>>i.second;
+    sort(menu.begin(),menu.end());
 
-    const ll INF = 1e18;
-    vector<vector<ll>> dp(N+1, vector<ll>(M+1, INF));
-    dp[0][0] = 0; // 0명 0개 메뉴는 비용 0
+    const ll INF = 1e15;
+    vector<vector<ll>> dp(N+1,vector<ll>(M+1,INF));
+    dp[0][0] = 0;
 
-    for (int idx=0; idx<M; idx++) {
-        ll A = menu[idx].first;
-        ll B = menu[idx].second;
-
-        // 뒤에서 앞으로 갱신 (같은 메뉴 중복 방지)
-        for (int n=N; n>=0; n--) {
-            for (int m=idx; m>=0; m--) {
-                if (dp[n][m] == INF) continue;
-
-                ll nnew = min((ll)N, n + B);
-                // 이 메뉴 추가 사용
-                dp[nnew][m+1] = min(dp[nnew][m+1],
-                                    dp[n][m] + (nnew - n) * A);
+    ll ans = 1e18;
+    for(int j=0;j<M;j++){
+        for(int i=N;i>=0;i--){ //idx
+            for(int k=0;k<=j;k++){
+                if(dp[i][k]==INF) continue;
+                int n = min(N,i+menu[j].second);
+                dp[n][k+1] = min(dp[n][k+1],dp[i][k]+(n-i)*menu[j].first);
+                //if(n==N) ans = min(ans,dp[n][k+1]*(k+1));
             }
         }
     }
+    /* for(auto i : dp){
+        for(auto j : i) cout<<j<<' ';
+        cout<<'\n';
+    }  */
+    for(int i=1;i<=M;i++) ans = min(ans,dp[N][i]*i);
+    cout<<ans;
 
-    ll ans = INF;
-    for (int m=1; m<=M; m++) {
-        if (dp[N][m] != INF) {
-            ans = min(ans, dp[N][m] * m);
-        }
-    }
-    cout << ans << "\n";
 }
