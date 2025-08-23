f8b64193fd342670cc68aeeb2ca8157e440e1d00
StuntJ <95505685+StuntJ@users.noreply.github.com>
2025-08-23 17:56:02 +0900

    [Bronze V] Title: 자동완성, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    char a;
+    cin>>a;
+    if(a=='N' || a=='n') cout<<"Naver D2";
+    else cout<<"Naver Whale";
+}
