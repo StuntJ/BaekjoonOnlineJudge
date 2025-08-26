7d1744cb3f50329d8b87e1c03bc4e880a377776d
StuntJ <95505685+StuntJ@users.noreply.github.com>
2025-08-27 00:32:37 +0900

    [Gold III] Title: 겨울이 좋아, Time: 52 ms, Memory: 4372 KB -BaekjoonHub

--- 백준/Gold/32984. 겨울이 좋아/겨울이 좋아.cc
+++ 백준/Gold/32984. 겨울이 좋아/겨울이 좋아.cc
@@ -2,44 +2,29 @@
 
 using namespace std;
 typedef long long ll;
-int main()
-{
-	ios::sync_with_stdio(0);
-	cin.tie(0);
 
-	int N;
-	cin >> N;
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
-	vector<ll> A(N), B(N);
-	for (auto& i : A) cin >> i;
-	for (auto& i : B) cin >> i;
-
-	vector<ll> remain(N);
-	for (int i = 0; i < N; i++)
-	{
-		remain[i] = A[i] / B[i];
-		if (A[i] % B[i] != 0) remain[i]++;
-	}
-
-	ll s = 0, e = 1000000000;
-	ll ans = 1000000000;
-	while (s <= e)
-	{
-		ll mid = (s + e) / 2;
-		ll sum = 0;
-		for (int i = 0; i < N; i++)
-		{
-			sum += max(remain[i] - mid, 0LL);
-		}
-		if (sum > mid)
-		{
-			s = mid + 1;
-		}
-		else
-		{
-			e = mid - 1;
-			ans = min(ans, mid);
-		}
-	}
-	cout << ans;
+    int N;
+    cin>>N;
+    vector<int> A(N),B(N),C(N);
+    for(auto &i : A) cin>>i;
+    for(auto &i : B) cin>>i;
+    for(int i=0;i<N;i++) C[i] = ((A[i]%B[i]) ? A[i]/B[i]+1 : A[i]/B[i]);
+    
+    ll s = 0, e = 1e9;
+    ll ans = 1e9;
+    while(s<=e){
+        ll mid = (s+e)/2;
+        ll sum = 0;
+        for(auto i : C) sum += max(0LL,i-mid);
+        if(sum>mid) s = mid+1;
+        else{
+            ans = min(ans,mid);
+            e = mid-1;
+        }
+    }
+    cout<<ans;
 }
