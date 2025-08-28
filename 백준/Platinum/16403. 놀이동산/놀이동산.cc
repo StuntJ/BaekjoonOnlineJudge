d24f44d38d34af6b85d0ce696bdbb7251bb7013b
StuntJ <95505685+StuntJ@users.noreply.github.com>
2025-08-28 22:04:29 +0900

    [Platinum V] Title: 놀이동산, Time: 0 ms, Memory: 2460 KB -BaekjoonHub

---
+++ 백준/Platinum/16403. 놀이동산/놀이동산.cc
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int N;
+    int A,B;
+    cin>>N;
+    cin>>A>>B;
+
+    vector<ll> v(N);
+    for(auto &i : v) cin>>i;
+    v.insert(v.begin(),0);
+    sort(v.begin(),v.end());
+    vector<ll> pSum(N+1);
+    for(int i=1;i<=N;i++) pSum[i] = pSum[i-1]+v[i];
+    const int INF = 1e9;
+    vector<ll> dp(N+1,INF);
+    dp[0] = 0;
+    for(int i=1;i<=N;i++){
+        dp[i] = min(dp[i],dp[i-1]+A*v[i]);
+        for(int j=i;j<=min(N,(i+39));j++){
+            int mid = (i+j)/2;
+            //if((v[j]-v[mid])*A>B || (v[mid]-v[i])*A>B) break;
+            ll res = 0;
+            if((j-i+1)&1) res+=v[mid];
+            res += -(pSum[mid]-pSum[i-1]) + (pSum[j]-pSum[mid]);
+            dp[j] = min(dp[j],dp[i-1]+res*A+B); 
+        }
+    }
+    /* for(int i=0;i<=N;i++) cout<<v[i]<<' ';
+    cout<<'\n';
+    for(int i=0;i<=N;i++) cout<<pSum[i]<<' ';
+    cout<<'\n';
+    for(int i=0;i<=N;i++) cout<<dp[i]<<' ';
+    cout<<'\n'; */
+    cout<<dp[N];
+}
