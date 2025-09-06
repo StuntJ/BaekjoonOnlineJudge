2011f853339593a0c3818dee0742e2985b9ebffb
StuntJ <95505685+StuntJ@users.noreply.github.com>
2025-09-06 20:55:47 +0900

    [Bronze IV] Title: SUAPC 의자 준비하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34183. SUAPC 의자 준비하기/SUAPC 의자 준비하기.cc
@@ -0,0 +1,43 @@
+    #include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<vector<int>>> vvvi;
+typedef vector<ll> vll;
+typedef vector<vector<ll>> vvll;
+typedef vector<vector<vector<ll>>> vvvll;
+typedef pair<int,int> pi;
+typedef pair<ll,ll> pl;
+typedef vector<pi> vpi;
+typedef vector<vector<pi>> vvpi;
+typedef vector<vector<vector<pi>>> vvvpi;
+typedef vector<double> vd;
+typedef vector<vd> vvd;
+typedef vector<vvd> vvvd;
+typedef vector<long double> vld;
+typedef vector<vld> vvld;
+typedef vector<vvld> vvvld;
+typedef vector<bool> vb;
+typedef vector<vector<bool>> vvb;
+typedef vector<vector<vector<bool>>> vvvb;
+typedef vector<char> vc;
+typedef vector<vc> vvc;
+typedef vector<vvc> vvvc;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int N,M,A,B;
+    cin>>N>>M>>A>>B;
+
+    int temp = max(0,N*3-M);
+    int ans = 0;
+    if(temp){
+        ans = temp*A+B;
+    } 
+    cout<<ans;
+}
