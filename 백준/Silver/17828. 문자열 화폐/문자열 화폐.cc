bb19e971daa593a9159ab1803718ef2473748827
StuntJ <95505685+StuntJ@users.noreply.github.com>
2025-09-13 21:23:11 +0900

    [Silver I] Title: 문자열 화폐, Time: 24 ms, Memory: 13668 KB -BaekjoonHub

---
+++ 백준/Silver/17828. 문자열 화폐/문자열 화폐.cc
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<vector<int>>> vvvi;
+typedef vector<ll> vll;
+typedef vector<vector<ll>> vvll;
+typedef vector<vector<vector<ll>>> vvvll;
+typedef pair<int,int> pi;
+typedef pair<ll,ll> pl;
+typedef vector<pi> vpi;
+typedef vector<vector<pi>> vvpi;
+typedef vector<vector<vector<pi>>> vvvpi;
+typedef vector<double> vd;
+typedef vector<vd> vvd;
+typedef vector<vvd> vvvd;
+typedef vector<long double> vld;
+typedef vector<vld> vvld;
+typedef vector<vvld> vvvld;
+typedef vector<bool> vb;
+typedef vector<vector<bool>> vvb;
+typedef vector<vector<vector<bool>>> vvvb;
+typedef vector<char> vc;
+typedef vector<vc> vvc;
+typedef vector<vvc> vvvc;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int N;
+    int X;
+    cin>>N>>X;
+
+    if(N*26<X || X<N){
+        cout<<"!";
+        return 0;
+    }
+
+    string str;
+    for(int i=0;i<N;i++){
+        int alpha = min(26,X-(N-i-1));
+        char a = 'A' - 1 + alpha;
+        str.push_back(a);
+        X-=alpha;
+    }
+    reverse(str.begin(),str.end());
+    cout<<str;
+}
