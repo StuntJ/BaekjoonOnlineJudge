b6333137aff72975bb971d632160c11e72896902
StuntJ <95505685+StuntJ@users.noreply.github.com>
2025-10-01 09:42:25 +0900

    [Silver I] Title: 도치균형의 배열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/34549. 도치균형의 배열/도치균형의 배열.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int M;
+    cin>>M;
+
+    for(int i=1;i<=M;i++) cout<<i<<' ';
+    for(int i=M-1;i>0;i--) cout<<i<<' ';
+}
