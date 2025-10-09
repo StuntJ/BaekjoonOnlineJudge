d5a5a9402b5c5a92f528f9c2981e55e8c2481a0a
StuntJ <95505685+StuntJ@users.noreply.github.com>
2025-10-09 15:09:37 +0900

    [Gold III] Title: 카드 게임, Time: 112 ms, Memory: 5976 KB -BaekjoonHub

--- 백준/Gold/11062. 카드 게임/카드 게임.cc
+++ 백준/Gold/11062. 카드 게임/카드 게임.cc
@@ -32,11 +32,5 @@ int main(){
 
         
         cout<<solve(solve,0,0)<<'\n';
-        /* for(int i=0;i<=N;i++){
-            for(int j=0;j<=N;j++){
-                cout<<dp[i][j]<< ' ';
-            }
-            cout<<'\n';
-        } */
     }
 }
