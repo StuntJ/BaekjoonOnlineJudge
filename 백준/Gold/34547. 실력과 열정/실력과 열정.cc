5e245dfaf95d49f121dbdc5b3119404238594fc2
StuntJ <95505685+StuntJ@users.noreply.github.com>
2025-10-12 20:48:59 +0900

    [Gold V] Title: 실력과 열정, Time: 132 ms, Memory: 4004 KB -BaekjoonHub

---
+++ 백준/Gold/34547. 실력과 열정/실력과 열정.cc
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    const int MAX_A = 1001;
+    int N;
+    cin>>N;
+
+    int A0,B0;
+    cin>>A0>>B0;
+    int K;
+    cin>>K;
+
+    vector<vector<int>> dp(N+1,vector<int>(MAX_A,-1));
+    dp[0][A0] = 0;
+    for(int i=0;i<N;i++){
+        for(int A=0;A<MAX_A;A++){
+            if(dp[i][A]==-1) continue;
+            int B = A0+B0-A;
+            for(int k=K;k<=A;k++){
+                dp[i+1][A-k] = max(dp[i+1][A-k],dp[i][A] + (A-k)*(B+k));
+            }
+            for(int k=K;k<=B;k++){
+                dp[i+1][A+k] = max(dp[i+1][A+k],dp[i][A] + (A+k)*(B-k));
+            }
+        }
+    }
+
+    int mx = 0;
+    for(int i=0;i<MAX_A;i++){
+        mx = max(dp[N][i],mx);
+    }
+    cout<<mx;
+}
