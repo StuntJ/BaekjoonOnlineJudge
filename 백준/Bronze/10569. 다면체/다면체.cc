45b4cd040670866ee12a738a8f423081645bf39a
StuntJ <95505685+StuntJ@users.noreply.github.com>
2025-12-04 23:22:57 +0900

    [Bronze IV] Title: 다면체, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10569. 다면체/다면체.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int T;
+    cin>>T;
+
+    while(T--){
+        int V,E;
+        cin>>V>>E;
+
+        cout<<2-V+E<<'\n';
+    }
+}
