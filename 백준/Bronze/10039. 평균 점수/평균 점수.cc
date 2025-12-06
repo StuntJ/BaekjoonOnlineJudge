3f1b59c992fb7b448d545c5d4a1a09410ffafa38
StuntJ <95505685+StuntJ@users.noreply.github.com>
2025-12-06 23:47:24 +0900

    [Bronze IV] Title: 평균 점수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10039. 평균 점수/평균 점수.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int sum = 0;
+    for(int i=0;i<5;i++){
+        int x;
+        cin>>x;
+        sum += max(40,x);
+    }
+    cout<<sum/5;
+}
