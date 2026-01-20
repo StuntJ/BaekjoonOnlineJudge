267bbc0d1bffd6625608ccf7b9cd0873f00e222a
StuntJ <95505685+StuntJ@users.noreply.github.com>
2026-01-20 22:05:01 +0900

    [Silver V] Title: 젓가락, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/24228. 젓가락/젓가락.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    ll N,R;
+    cin>>N>>R;
+
+    cout<<N+1+2*(R-1);
+}
