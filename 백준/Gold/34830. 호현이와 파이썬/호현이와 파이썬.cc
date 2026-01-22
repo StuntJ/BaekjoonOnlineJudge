c3a464904e6c9f5a1e67365bdf64224bdbe478df
StuntJ <95505685+StuntJ@users.noreply.github.com>
2026-01-22 21:40:05 +0900

    [Gold III] Title: 호현이와 파이썬, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/34830. 호현이와 파이썬/호현이와 파이썬.cc
+++ 백준/Gold/34830. 호현이와 파이썬/호현이와 파이썬.cc
@@ -6,11 +6,6 @@ typedef long long ll;
 int main(){
     ll N;
     cin>>N;
-
-    if(N&1){
-        cout<<N*(N-1)/2<<'\n';
-    }
-    else{
-        cout<<N*(N-1)/2+(N-2)/2<<'\n';
-    }
+    if(N&1) cout<<N*(N-1)/2<<'\n';
+    else cout<<N*(N-1)/2+(N-2)/2;
 }
