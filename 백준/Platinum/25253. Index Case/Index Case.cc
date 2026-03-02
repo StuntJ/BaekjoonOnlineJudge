a69e1042fa80ceb79cf27f3032863c57566fcfb5
StuntJ <95505685+StuntJ@users.noreply.github.com>
2026-03-02 23:17:54 +0900

    [Platinum V] Title: Index Case, Time: 20 ms, Memory: 2052 KB -BaekjoonHub

--- 백준/Platinum/25253. Index Case/Index Case.cc
+++ 백준/Platinum/25253. Index Case/Index Case.cc
@@ -6,22 +6,16 @@ int f[11][11][11];
 int s0[201];
 bool dp[201][11][11];
 
-bool check(int first, int second) {
-    // 매번 DP 테이블 초기화
+bool check(int first, int second){
     memset(dp, 0, sizeof(dp));
-    
-    // 초기 상태: s-1의 1번 칸이 first, 2번 칸이 second일 때
     dp[2][first][second] = true;
 
-    for (int i = 2; i < n; i++) {
-        for (int cur = 1; cur <= m; cur++) {
-            for (int nxt = 1; nxt <= m; nxt++) {
+    for (int i = 2; i < n; i++){
+        for (int cur = 1; cur <= m; cur++){
+            for (int nxt = 1; nxt <= m; nxt++){
                 if (!dp[i][cur][nxt]) continue;
-                
-                // s-1의 i+1번째 칸에 올 수 있는 후보 val을 찾음
-                for (int val = 1; val <= m; val++) {
-                    // s0[i] = f(s-1[i-1], s-1[i], s-1[i+1])
-                    if (f[cur][nxt][val] == s0[i]) {
+                for (int val = 1; val <= m; val++){
+                    if (f[cur][nxt][val] == s0[i]){
                         dp[i + 1][nxt][val] = true;
                     }
                 }
@@ -29,15 +23,10 @@ bool check(int first, int second) {
         }
     }
 
-    // 마지막 연결 확인 (원형 구조)
-    // dp[n][cur][last]가 참이라면, s-1[n-1]=cur, s-1[n]=last 인 상태임
-    for (int cur = 1; cur <= m; cur++) {
-        for (int last = 1; last <= m; last++) {
-            if (dp[n][cur][last]) {
-                // 마지막 조건 2개 체크:
-                // s0[n] = f(s-1[n-1], s-1[n], s-1[1])
-                // s0[1] = f(s-1[n], s-1[1], s-1[2])
-                if (f[cur][last][first] == s0[n] && f[last][first][second] == s0[1]) {
+    for (int cur = 1; cur <= m; cur++){
+        for (int last = 1; last <= m; last++){
+            if (dp[n][cur][last]){
+                if (f[cur][last][first] == s0[n] && f[last][first][second] == s0[1]){
                     return true;
                 }
             }
@@ -46,8 +35,8 @@ bool check(int first, int second) {
     return false;
 }
 
-void solve() {
-    if (!(cin >> n >> m)) return;
+void solve(){
+    cin>>n>>m;
     for (int i = 1; i <= m; i++)
         for (int j = 1; j <= m; j++)
             for (int k = 1; k <= m; k++)
@@ -55,16 +44,15 @@ void solve() {
 
     for (int i = 1; i <= n; i++) cin >> s0[i];
 
-    // s-1의 첫 두 칸(1번, 2번)을 모든 경우의 수(M*M)로 시도
-    for (int i = 1; i <= m; i++) {
-        for (int j = 1; j <= m; j++) {
+    for (int i = 1; i <= m; i++){
+        for (int j = 1; j <= m; j++){
             if (check(i, j)) {
-                cout << "yes" << endl;
+                cout << "yes" << '\n';
                 return;
             }
         }
     }
-    cout << "no" << endl;
+    cout << "no" << '\n';
 }
 
 int main(){
