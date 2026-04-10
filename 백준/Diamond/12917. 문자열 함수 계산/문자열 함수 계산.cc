4928dc5a8d08e8a54ddb67384b67e4a59e09942d
StuntJ <95505685+StuntJ@users.noreply.github.com>
2026-04-10 11:46:14 +0900

    [Diamond V] Title: 문자열 함수 계산, Time: 20 ms, Memory: 5024 KB -BaekjoonHub

---
+++ 백준/Diamond/12917. 문자열 함수 계산/문자열 함수 계산.cc
@@ -0,0 +1,104 @@
+#include <bits/stdc++.h>
+#define all(x) (x).begin(),(x).end()
+
+using namespace std;
+typedef long long ll;
+
+pair<vector<int>,vector<int>> SuffixArray(const string &s) {
+    int n = s.size(), m = max(n,256);
+    vector<int> sa(n),lcp(n),pos(n),tmp(n),cnt(m);
+    auto counting_sort = [&]() {
+        fill(cnt.begin(),cnt.end(),0);
+        for (int i=0;i<n;i++) cnt[pos[i]]++;
+        partial_sum(cnt.begin(),cnt.end(),cnt.begin());
+
+        for (int i=n-1;i>=0;i--) sa[--cnt[pos[tmp[i]]]] = tmp[i];
+    };
+    for (int i=0;i<n;i++) sa[i] = i, pos[i] = s[i], tmp[i] = i;
+    counting_sort();
+    for (int k = 1; ; k<<=1) {
+        int p = 0;
+        for (int i=n-k;i<n;i++) tmp[p++] = i;
+        for (int i=0;i<n;i++) if (sa[i]>=k) tmp[p++] = sa[i]-k;
+        counting_sort();
+        tmp[sa[0]] = 0;
+        for (int i=1;i<n;i++) {
+            tmp[sa[i]] = tmp[sa[i-1]];
+            if (sa[i-1]+k<n&&sa[i]+k<n&&pos[sa[i-1]]==pos[sa[i]]&&pos[sa[i-1]+k]==pos[sa[i]+k]) continue;
+            tmp[sa[i]] +=1;
+        }
+        swap(pos,tmp); if (pos[sa.back()]+1==n) break;
+    }
+    for (int i=0,j=0;i<n;i++,j=max(j-1,0)) {
+        if (pos[i]==0) continue;
+        while (sa[pos[i]-1]+j<n&&sa[pos[i]]+j<n&&s[sa[pos[i]-1]+j]==s[sa[pos[i]]+j])
+            j++;
+        lcp[pos[i]] = j;
+    }
+
+    return {sa,lcp};
+}
+
+void solve() {
+    string T;
+    cin>>T;
+
+    int N = T.size();
+
+    auto [sa,lcp] = SuffixArray(T);
+
+    // for (auto &i : lcp) cout<<i<<' ';
+    // cout<<'\n';
+
+    vector<int> left(N), right(N);
+
+    stack<int> st;
+    for (int i=0;i<N;i++) {
+        while (!st.empty() && lcp[st.top()]>lcp[i]) {
+            right[st.top()] = i;
+            st.pop();
+        }
+
+        st.push(i);
+    }
+
+    while (!st.empty()) {
+        right[st.top()] = N;
+        st.pop();
+    }
+
+    for (int i=N-1;i>=0;i--) {
+        while (!st.empty() && lcp[st.top()]>lcp[i]) {
+            left[st.top()] = i;
+            st.pop();
+        }
+
+        st.push(i);
+    }
+
+    while (!st.empty()) {
+        left[st.top()] = -1;
+        st.pop();
+    }
+
+    // for (auto &i : right) cout<<i<<' ';
+    // cout<<'\n';
+    //
+    // for (auto &i : left) cout<<i<<' ';
+    // cout<<'\n';
+
+    ll mx = N;
+    for (int i=0;i<N;i++) {
+        mx = max(mx, (ll)lcp[i] * (right[i]-left[i]));
+    }
+
+    cout<<mx;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t = 1; //cin>>t;
+    while (t--) solve();
+}
