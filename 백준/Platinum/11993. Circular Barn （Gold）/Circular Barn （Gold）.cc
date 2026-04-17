7343c5a75d6f40b9c1e3198da00ac3eaa059018f
StuntJ <95505685+StuntJ@users.noreply.github.com>
2026-04-17 15:21:36 +0900

    [Platinum IV] Title: Circular Barn (Gold), Time: 8 ms, Memory: 2544 KB -BaekjoonHub

---
+++ 백준/Platinum/11993. Circular Barn （Gold）/Circular Barn （Gold）.cc
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+void solve() {
+    int N;
+    cin>>N;
+
+    vector<int> v(N);
+    for (auto &i : v) cin>>i;
+    vector<bool> chk(N);
+
+    int idx = 0;
+    int st = 0;
+    for (int i=0;i<N;i++) {
+        if (i==0||!chk[i-1]) {
+            st = i;
+            idx = i;
+        }
+
+        bool flag = false;
+        int oridx = idx;
+        for (idx = oridx; idx<oridx+v[i];idx++) {
+            if (!chk[idx%N]) chk[idx%N] = true;
+            else {
+                flag = true;
+            }
+            if (flag)
+                break;
+        }
+
+        if (flag) break;
+    }
+
+    ll sum = 0;
+    idx = st;
+    //cout<<st<<'\n';
+    int orp = st;
+    for (int p = st; p<orp+N; p++) {
+        int oridx = idx;
+        for (idx = oridx ; idx<oridx+v[p%N];idx++) {
+            sum += (ll)(idx-p)*(idx-p);
+        }
+    }
+
+    cout<<sum;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t = 1; //cin>>t;
+    while (t--) solve();
+}
