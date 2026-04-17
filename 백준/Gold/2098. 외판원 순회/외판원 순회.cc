7c15c2055a257157d3b75412691c1bbb5f5f61c3
StuntJ <95505685+StuntJ@users.noreply.github.com>
2026-04-18 00:36:28 +0900

    [Gold I] Title: 외판원 순회, Time: 20 ms, Memory: 6436 KB -BaekjoonHub

--- 백준/Gold/2098. 외판원 순회/외판원 순회.cc
+++ 백준/Gold/2098. 외판원 순회/외판원 순회.cc
@@ -1,50 +1,75 @@
-#include <iostream>
-#include <vector>
-#define INF 987654321
+#include <bits/stdc++.h>
 
 using namespace std;
+typedef long long ll;
+const int INF = 1e9;
 
-int N;
-vector<vector<int>> adj;
-vector<vector<int>> cache;
-
-int TSP(int here,int visited)
-{
-    if(visited==(1<<N)-1) return adj[here][0];
-    int &ret = cache[here][visited];
-    if(ret!=-1) return ret;
-    ret = INF;
-
-    for(int i=0;i<N;i++)
-    {
-        if(adj[here][i]==INF) continue;
-        if(!(visited&(1<<i)))
-        {
-            ret = min(ret,adj[here][i]+TSP(i,visited|(1<<i)));
-        }
-    }
-    return ret;
-}
-
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
+void solve() {
+    int N;
     cin>>N;
 
-    adj.resize(N+1,vector<int>(N+1));
-    cache.assign(N+1,vector<int>(1<<N,-1));
+    vector<vector<int>> adj(N,vector<int>(N));
 
-    for(int i=0;i<N;i++)
-    {
-        for(int j=0;j<N;j++)
-        {
-            cin>>adj[i][j];
-            if(i!=j&&adj[i][j]==0) adj[i][j] = INF;
+    for (auto &i : adj) for (auto &j : i) cin>>j;
+
+    for (int i=0;i<N;i++) for (int j=0;j<N;j++) if (i!=j && adj[i][j]==0) adj[i][j] = INF;
+
+    int size = (1<<(N-1));
+    vector<vector<int>> dp(N,vector<int>(size,INF)); //v_j->v_x->v_x->v_x->v_1
+    vector<vector<int>> P(N,vector<int>(size,-1));
+
+    for (int i=1;i<N;i++) {
+        dp[i][0] = adj[i][0]; //v_j->v_1 설정하고 가운데 끼워 넣는거
+    }
+
+    for (int k=1;k<=N-2;k++) {
+        for (int stat = 1; stat<size; stat++) {
+            int cnt = __builtin_popcount(stat);
+            if (cnt==k) {
+                for (int i=1;i<N;i++) {
+                    if (!(stat&(1<<(i-1)))) {
+                        for (int j=1;j<N;j++) {
+                            if (stat&(1<<(j-1))) {
+                                int prev = stat&~(1<<(j-1));
+                                if (dp[i][stat]>dp[j][prev]+adj[i][j]) {
+                                    dp[i][stat] = dp[j][prev]+adj[i][j];
+                                    P[i][stat] = j; //i, stat일때 j로 전이하는게 이득
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 
-    cout<<TSP(0,1);
+    for (int i=1;i<N;i++) { //v_i->...->v_0 을 이용하여 v_0->v_i->...v_0
+        int full = size-1;
+        int prev = full & ~(1<<(i-1));
+
+        if (dp[0][full]>dp[i][prev]+adj[0][i]) {
+            dp[0][full] = dp[i][prev]+adj[0][i];
+            P[0][full] = i;
+        }
+    }
+
+    cout<<dp[0][size-1]<<'\n';
+    
+    // function<void(int,int)> f = [&](int here, int stat) {
+    //     cout<<here<<"->";
+    //     int next = P[here][stat];
+    //     if (next!=-1)
+    //         f(next,stat&~(1<<(next-1)));
+    // };
+    //
+    // f(0,size-1);
+    // cout<<0<<'\n';
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t = 1; //cin>>t;
+    while (t--) solve();
 }
